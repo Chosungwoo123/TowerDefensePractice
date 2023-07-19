167f164339bbd201cd358d29a70d91b8695ad912
Chosungwoo123 <ejyoo12345@naver.com>
2023-07-20 01:18:53 +0900

    적 체력바 구현

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -2,16 +2,24 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float maxHealth;
+
+    [SerializeField] private Image healthImage;
 
     private float curHealth;
 
     private void Start()
     {
         curHealth = maxHealth;
+    }
+
+    private void Update()
+    {
+        healthImage.fillAmount = Mathf.Lerp(healthImage.fillAmount, curHealth / maxHealth, Time.deltaTime * 4);
     }
 
     public void OnDamage(float damage)
